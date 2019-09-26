b21721d57d0ab834fae207bc8b35dfc58ea4065e
Ish Mohan Gupta <img5156@comp-hd-001.gwave.ics.psu.edu>
2019-09-26 13:11:23 -0400

    debugging

--- GW170817binningv3.py
+++ GW170817binningv3.py
@@ -53,7 +53,7 @@ psd_L = 2.0*np.convolve(np.absolute(LFT)**2, np.ones((n_conv))/n_conv, mode='sam
 psd_H = 2.0*np.convolve(np.absolute(HFT)**2, np.ones((n_conv))/n_conv, mode='same')*T
 psd = [psd_L, psd_H]
 
-pl.plot(f,psd_L)
+pl.loglog(f,psd_L)
 pl.savefig('figures/plot_psd_t_full.pdf')
 pl.close()
 
