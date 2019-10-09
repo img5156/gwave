a4b3570ae2ad35e88db6e6e50fe3b5d95d266f38
Ish Mohan Gupta <img5156@comp-hd-001.gwave.ics.psu.edu>
2019-10-09 18:30:44 -0400

    debugged

--- overlap.py
+++ overlap.py
@@ -61,7 +61,8 @@ TC2 = -127.5521
 
 # fiducial waveforms sampled at full frequency resolution
 h0 = hf3hPN(f, M, ETA, s1z=S1Z, s2z=S2Z, Lam=LAM)
-h1 = h0*np.exp(-2.0j*np.pi*f*TC1)
+#h1 = h0*np.exp(-2.0j*np.pi*f*TC1)
+h1 = h0
 h1[0] = 0
 print('Constructed fiducial waveforms.')
 
@@ -69,10 +70,10 @@ print('Constructed fiducial waveforms.')
 # range of frequency to be used in the computation of likelihood [f_lo, f_hi] [Hz]
 f_lo = 23.0
 f_hi = 1000.0
-tau = (5.0/(256*np.pi*f_lo))*((np.pi*MC*f_lo)**(-5/3))
-
+tau = (5.0/(256.*np.pi*f_lo))*((np.pi*MC*5.*(10.**(-6.))*f_lo)**(-5./3.))
+print("tau=",tau)
 Nbin, fbin, fbin_ind = setup_bins(f_full=f, f_lo=f_lo, f_hi=f_hi, chi=1.0, eps=0.5)
-print(Nbin, fbin, fbin_ind)
+#print(Nbin, fbin, fbin_ind)
 
 print("Frequency binning done: # of bins = %d"%(Nbin))
 
@@ -83,21 +84,33 @@ rL = compute_rf(parL, h1, fbin, fbin_ind)
 #h_int = [np.zeros(len(f)), np.zeros(len(f))]
 h_int = np.array(np.zeros(len(f)), dtype=np.complex128)
 j = fbin_ind[0]
-#ad = round(mt.floor(T/(2*tau)))
-ad = 1
+m = 1./(2.*tau)
+df = 1./T
+ad = m/df
+print("ad=",ad)
+#ad = 1
 fp = np.array(np.zeros(len(f)))
+
+#m = 0
+#for i in range(len(fbin)-1):
+#  for fn in np.arange(f[fbin_ind[i]], f[fbin_ind[i+1]], 1/(2*tau)):
+#    fp[m] = fn
+#    m+=1
+
+print("Frequency array prepared")
+k = 0
 for i in range(len(fbin)-1):
-  fmid = 0.5*(f[fbin_ind[i]] + f[fbin_ind[i+1]])
-  for fn in np.arange(f[fbin_ind[i]], f[fbin_ind[i+1]], 1/(2*tau)):
-    k = j-fbin_ind[0]
+  fmid = 0.5*(fbin[i] + fbin[i+1])
+  for fn in np.arange(f[fbin_ind[i]], f[fbin_ind[i+1]], m):
+    #k = j-fbin_ind[0]
     fp[k] = fn
-    #fh = fbin_ind[i]+(j-fbin_ind[i]+1)*ad
-    fh = fbin_ind[i]*(1-ad)+j*ad
-    h = 0.5*(h1[fh]+h1[fh+ad])
+    fh = fbin_ind[i]+int((j-fbin_ind[i])*ad)
+    #fh = fbin_ind[i]*(1-int(ad))+int((j+1)*ad)
+    h = 0.5*(h1[fh]+h1[fh+1])
     h_int[k] = (rL[0][i] + (fn-fmid)*rL[1][i])*h
     j+=1
-      
-  print(i)
+    k+=1  
+  #print(i)
 
 fp = np.asarray(fp[:k])  
 psd = sh(fp)
@@ -116,9 +129,9 @@ c = np.absolute(overlap(h2,h_int,fp))
 
 d = c/(np.sqrt(a)*np.sqrt(b))
 
-print("h=",h2[1:1000])
-print("h_int=",h_int[1:1000])
-print("psd_L",psd[0:100])
+#print("h=",h2[1:1000])
+#print("h_int=",h_int[1:1000])
+#print("psd_L",psd[0:100])
 print("Overlap1=",a)
 print("Overlap2=",b)
 print("Overlap3=",c)
