61ffceb220a3feba420b35a29050c443d4dfdfb5
img5156 <55452901+img5156@users.noreply.github.com>
2019-10-10 11:11:51 -0400

    Update overlap_Mc.py

--- overlap_Mc.py
+++ overlap_Mc.py
@@ -1,244 +1,134 @@
 import numpy as np
 import scipy as sp
-from scipy import interpolate
-from scipy.optimize import minimize
+import math as mt
+# provide sample waveform model
+from waveform_o import *
+# routines for binning, summary data, etc.
+from binning import *
+
+# number of detectors (Livingston and Hanford)
+#ndtct = 1
+
+n_sample = 2**20
+T = 256.0
+#n_conv = 20
+
+# load LIGO strain data (time domain)
+#L1 = np.loadtxt('data/L.txt')
+#H1 = np.loadtxt('data/H.txt')
 
 
-# need to provide a waveform-generating function which takes an array of frequencies as input and generate h(f)
-# one can replace this function with whatever waveform generating function one would like to use
+print('Finished loading LIGO data.')
 
-# ---------- Analytical TaylorF2 frequency-domain waveform -------------- #
-# including aligned spin effects
-# tidal effects added
-# circular binary 3.5PN GW phase
-# wave frequency f [Hz]
-# total mass M [Msun]
-# eta is the symmetric mass ratio
-# we adopt the convention that m1 >= m2
-# Lam is the reduced tidal parameter \tilde\Lam
-# aligned spin components: s1z and s2z
-# chieff is the effective spin parameters: chieff = (m1*s1z + m2*s2z)/(m1 + m2)
-# delta is the asymmetric mass ratio: delta = (m1 - m2)/(m1 + m2)
-# By default we neglect spin and tidal deformation
+# time-domain and frequency-domain grid
+t = np.linspace(0, T, n_sample+1)[:-1]
+f = np.linspace(0, 1.0/T*n_sample/2.0, n_sample//2+1)
 
-# step function
-def myHeaviside(x):
-    return np.heaviside(x, 1.0)
+# apply a Tukey window function to eliminate the time-domain boundary ringing
+#tukey = sp.signal.tukey(n_sample, alpha=0.1)
+#pl.plot(t,tukey)
+#L1T = L1*tukey
+#pl.plot(t,L1T)
+#pl.plot(t,L1)
+#LFT = np.fft.rfft(L1T)/n_sample
 
+# estimate PSDs for both L1 and H1
+#psd_L = 2.0*np.convolve(np.absolute(LFT)**2, np.ones((n_conv))/n_conv, mode='same')*T
 
-# phase of h(f)
-def Phif3hPN(f, M, eta, phi_c, s1x=0.0, s1y=0.0, s1z=0.0, s2x=0.0, s2y=0.0, s2z=0.0, Lam=0.0, dLam=0.0):
-    gt = 4.92549094830932e-6  # GN*Msun/c^3 in seconds
-    EulerGamma = 0.57721566490153286060
-    vlso = 1.0 / np.sqrt(6.0)
-    delta = np.sqrt(1.0 - 4.0 * eta)
-    v = (np.pi * M * (f + 1e-100) * gt) ** (1.0 / 3.0)
-    v2 = v * v
-    v3 = v2 * v
-    v4 = v2 * v2
-    v5 = v4 * v
-    v6 = v3 * v3
-    v7 = v3 * v4
-    v10 = v5 * v5
-    v12 = v10 * v2
-    eta2 = eta ** 2
-    eta3 = eta ** 3
+def sh(f):
+    s = 5.623746655206207e-51 + 6.698419551167371e-50*f**(-0.125) + 7.805894950092525e-31/f**20. + 4.35400984981997e-43/f**6. + 1.630362085130558e-53*f + 2.445543127695837e-56*f**2 + 5.456680257125753e-66*f**5
+    return s
 
-    m1M = 0.5 * (1.0 + delta)
-    m2M = 0.5 * (1.0 - delta)
-    chi1L = s1z
-    chi2L = s2z
-    chi1sq = s1x * s1x + s1y * s1y + s1z * s1z
-    chi2sq = s2x * s2x + s2y * s2y + s2z * s2z
-    chi1dotchi2 = s1x * s2x + s1y * s2y + s1z * s2z
-    SL = m1M * m1M * chi1L + m2M * m2M * chi2L
-    dSigmaL = delta * (m2M * chi2L - m1M * chi1L)
+def overlap(A, B, f):
+    summ = 2.*np.real((((A*np.conjugate(B)+np.conjugate(A)*B)/psd).sum()))*res
+    return summ
 
-    # phase correction due to spins
-    sigma = eta * (721.0 / 48.0 * chi1L * chi2L - 247.0 / 48.0 * chi1dotchi2)
-    sigma += 719.0 / 96.0 * (m1M * m1M * chi1L * chi1L + m2M * m2M * chi2L * chi2L)
-    sigma -= 233.0 / 96.0 * (m1M * m1M * chi1sq + m2M * m2M * chi2sq)
-    phis_15PN = 188.0 * SL / 3.0 + 25.0 * dSigmaL
-    ga = (554345.0 / 1134.0 + 110.0 * eta / 9.0) * SL + (13915.0 / 84.0 - 10.0 * eta / 3.0) * dSigmaL
-    pn_ss3 = (326.75 / 1.12 + 557.5 / 1.8 * eta) * eta * chi1L * chi2L
-    pn_ss3 += ((4703.5 / 8.4 + 2935.0 / 6.0 * m1M - 120.0 * m1M * m1M) + (
-                -4108.25 / 6.72 - 108.5 / 1.2 * m1M + 125.5 / 3.6 * m1M * m1M)) * m1M * m1M * chi1sq
-    pn_ss3 += ((4703.5 / 8.4 + 2935.0 / 6.0 * m2M - 120.0 * m2M * m2M) + (
-                -4108.25 / 6.72 - 108.5 / 1.2 * m2M + 125.5 / 3.6 * m2M * m2M)) * m2M * m2M * chi2sq
-    phis_3PN = np.pi * (3760.0 * SL + 1490.0 * dSigmaL) / 3.0 + pn_ss3
-    phis_35PN = (-8980424995.0 / 762048.0 + 6586595.0 * eta / 756.0 - 305.0 * eta2 / 36.0) * SL \
-                - (170978035.0 / 48384.0 - 2876425.0 * eta / 672.0 - 4735.0 * eta2 / 144.0) * dSigmaL
+print('Finished estimating the PSD.')
 
-    # tidal correction to phase
-    # Lam is the reduced tidal deformation parameter \tilde\Lam
-    # dLam is the asymmetric reduced tidal deformation parameter, which is usually negligible
-    tidal = Lam * v10 * (- 39.0 / 2.0 - 3115.0 / 64.0 * v2) + dLam * 6595.0 / 364.0 * v12
-
-    return  phi_c + 3.0 / 128.0 / eta / v5 * (
-                1.0 + 20.0 / 9.0 * (743.0 / 336.0 + 11.0 / 4.0 * eta) * v2 + (phis_15PN - 16.0 * np.pi) * v3 \
-                + 10.0 * (3058673.0 / 1016064.0 + 5429.0 / 1008.0 * eta + 617.0 / 144.0 * eta2 - sigma) * v4 \
-                + (38645.0 / 756.0 * np.pi - 65.0 / 9.0 * eta * np.pi - ga) * (1.0 + 3.0 * np.log(v / vlso)) * v5 \
-                + (11583231236531.0 / 4694215680.0 - 640.0 / 3.0 * np.pi ** 2 - 6848.0 / 21.0 * (
-                    EulerGamma + np.log(4.0 * v)) + \
-                   (
-                               -15737765635.0 / 3048192.0 + 2255.0 * np.pi ** 2 / 12.0) * eta + 76055.0 / 1728.0 * eta2 - 127825.0 / 1296.0 * eta3 + phis_3PN) * v6 \
-                + (np.pi * (
-                    77096675.0 / 254016.0 + 378515.0 / 1512.0 * eta - 74045.0 / 756.0 * eta ** 2) + phis_35PN) * v7 +tidal)
+MC = 1.1976                                      # detector frame chirp mass [Msun]
+ETA = 0.244                                      # symmetric mass ratio m1*m2/(m1 + m2)**2
+DELTA = np.sqrt(1.0 - 4.0*ETA)                   # asymmetric mass ratio (m1 - m2)/(m1 + m2)
+M = MC/ETA**0.6                                  # total mass [Msun]
+M1 = 0.5*M*(1.0 + DELTA)                        # primary mass [Msun]
+M2 = 0.5*M*(1.0 - DELTA)                        # second mass [Msun]
+S1Z = 0.0                                        # aligned spin component for the primary
+S2Z = 0.0                                        # aligned spin component for the secondary
+CHIA = 0.5*(S1Z - S2Z)
+CHIS = 0.5*(S1Z + S2Z)
+CHIEFF = CHIS + DELTA*CHIA
+LAM = 0.0                                      # reduced tidal deformation parameter
+TC1 = -127.5556                                  # merger time (L1)
 
 
-# correction to modulus of h(f)
-# normalization should be C * (GN*Mchirp/c^3)^5/6 * f^-7/6 / D / pi^2/3 * (5/24)^1/2
-def Af3hPN(f, M, eta, s1x=0.0, s1y=0.0, s1z=0.0, s2x=0.0, s2y=0.0, s2z=0.0, Lam=0.0, dLam=0.0):
-    gt = 4.92549094830932e-6  # GN*Msun/c^3 in seconds
-    EulerGamma = 0.57721566490153286060
-    delta = np.sqrt(1.0 - 4.0 * eta)
-    v = (np.pi * M * (f + 1e-100) * gt) ** (1.0 / 3.0)
-    v2 = v * v
-    v3 = v2 * v
-    v4 = v2 * v2
-    v5 = v4 * v
-    v6 = v3 * v3
-    v7 = v3 * v4
-    eta2 = eta ** 2
-    eta3 = eta ** 3
+# fiducial waveforms sampled at full frequency resolution
+h0 = hf3hPN(f, M, ETA, 0.0, s1z=S1Z, s2z=S2Z, Lam=LAM)
+#h1 = h0*np.exp(2.0j*np.pi*f*TC1)
+h1 = h0
+h1[0] = 0
+print('Constructed fiducial waveforms.')
 
-    # modulus correction due to aligned spins
-    chis = 0.5 * (s1z + s2z)
-    chia = 0.5 * (s1z - s2z)
-    be = 113.0 / 12.0 * (chis + delta * chia - 76.0 / 113.0 * eta * chis)
-    sigma = chia ** 2 * (81.0 / 16.0 - 20.0 * eta) + 81.0 / 8.0 * chia * chis * delta + chis ** 2 * (
-                81.0 / 16.0 - eta / 4.0)
-    eps = delta * chia * (502429.0 / 16128.0 - 907.0 / 192.0 * eta) + chis * (
-                5.0 / 48.0 * eta2 - 73921.0 / 2016.0 * eta + 502429.0 / 16128.0)
+# prepare frequency binning
+# range of frequency to be used in the computation of likelihood [f_lo, f_hi] [Hz]
+f_lo = 23.0
+f_hi = 1000.0
+Nbin, fbin, fbin_ind = setup_bins(f_full=f, f_lo=f_lo, f_hi=f_hi, chi=1.0, eps=0.5)
+print(Nbin, fbin, fbin_ind)
 
-    return 1.0 + v2 * (11.0 / 8.0 * eta + 743.0 / 672.0) + v3 * (be / 2.0 - 2.0 * np.pi) + v4 * (
-                1379.0 / 1152.0 * eta2 + 18913.0 / 16128.0 * eta + 7266251.0 / 8128512.0 - sigma / 2.0) \
-           + v5 * (57.0 / 16.0 * np.pi * eta - 4757.0 * np.pi / 1344.0 + eps) \
-           + v6 * (
-                       856.0 / 105.0 * EulerGamma + 67999.0 / 82944.0 * eta3 - 1041557.0 / 258048.0 * eta2 - 451.0 / 96.0 * np.pi ** 2 * eta + 10.0 * np.pi ** 2 / 3.0 \
-                       + 3526813753.0 / 27869184.0 * eta - 29342493702821.0 / 500716339200.0 + 856.0 / 105.0 * np.log(
-                   4.0 * v)) \
-           + v7 * (- 1349.0 / 24192.0 * eta2 - 72221.0 / 24192.0 * eta - 5111593.0 / 2709504.0) * np.pi
+print("Frequency binning done: # of bins = %d"%(Nbin))
 
+#tau = (5.0/(256.*np.pi*f_lo))*((np.pi*MC*5.*(10.**(-6.))*f_lo)**(-5./3.))
+#print("tau=",tau)
 
-# combine the modulus and the phase of h(f)
-def hf3hPN(f, M, eta, phi_c, s1x=0.0, s1y=0.0, s1z=0.0, s2x=0.0, s2y=0.0, s2z=0.0, Lam=0.0, dLam=0.0, Deff=1.0):
-    pre = 3.6686934875530996e-19  # (GN*Msun/c^3)^(5/6)/Hz^(7/6)*c/Mpc/sec
-    Mchirp = M * eta ** 0.6
-    A0 = Mchirp ** (5.0 / 6.0) / (f + 1e-100) ** (7.0 / 6.0) / Deff / np.pi ** (2.0 / 3.0) * np.sqrt(5.0 / 24.0)
+MA = np.linspace(1.1973,1.1978,100)
+fp = np.array(np.zeros(len(f)))
+h_int = np.array(np.zeros(len(f)), dtype=np.complex128)
+fp = np.array(np.zeros(len(MA)))
 
-    Phi = Phif3hPN(f, M, eta, phi_c, s1x=s1x, s1y=s1y, s1z=s1z, s2x=s2x, s2y=s2y, s2z=s2z, Lam=Lam, dLam=dLam)
-    A = Af3hPN(f, M, eta, s1x=s1x, s1y=s1y, s1z=s1z, s2x=s2x, s2y=s2y, s2z=s2z, Lam=Lam, dLam=dLam)
+for l in range(len(MA)):
+    parL = [MA[l], ETA, CHIEFF, CHIA, LAM, TC1]
+    rL = compute_rf(parL, h1, fbin, fbin_ind)
+    tau = (5.0/(256.*np.pi*f_lo))*((np.pi*MC*5.*(10.**(-6.))*f_lo)**(-5./3.))
 
-    # note the convention for the sign in front of the phase
-    return pre * A0 * A * np.exp(-1.0j * Phi)
+    res = 1./(2.*tau)
+    df = 1./T
+    ad = res/df
+    M = MA[l]/ETA**0.6
 
+    k = 0
+    j = fbin_ind[0]
 
-# compute matched filter SNR^2
-def mfSNR2_amp_phic_max_sdat(sdat, k, rdata):
-    """
-    Compute matched filtering SNR^2 for the k-th detector using summary data: <sFT[k]|hFT>^2 / <hFT|hFT>
-    This is maximized with respect to an arbitrary normalization of the template, as well as an arbitrary phase
-    Summary data is passed through: sdat = [sdat_A0, sdat_A1, sdat_B0, sdat_B1]; each entry is a list for all detectors
-    rdata = [r0, r1] is the relative waveform sampled at the bin edges
-    """
-    r0, r1 = rdata
-    denom = np.sum(sdat[2][k] * np.absolute(r0) ** 2 + sdat[3][k] * 2.0 * (r0 * np.conjugate(r1)).real)
-    X = np.sum(sdat[0][k] * np.conjugate(r0) + sdat[1][k] * np.conjugate(r1))
-    res = np.absolute(X) ** 2 / denom
-    # print('denom = ', denom)
-    # print('overlap = ', res)
-    return res
+    for i in range(len(fbin)-1):
+      fmid = 0.5*(fbin[i] + fbin[i+1])
+      for fn in np.arange(f[fbin_ind[i]], f[fbin_ind[i+1]], res):    
+        fp[k] = fn
+        fh = fbin_ind[i]+int((j-fbin_ind[i])*ad)
+        h = 0.5*(h1[fh]+h1[fh+1])
+        h_int[k] = (rL[0][i] + (fn-fmid)*rL[1][i])*h
+        j+=1
+        k+=1  
 
+    #Truncating the array to appropriate size
+    fp = fp[:k] 
+    h_int = h_int[:k]
 
-# compute relative waveform r(f) = h(f)/h0(f)
-def compute_rf(par, h0, fbin, fbin_ind):
-    """
-    compute the ratio r(f) = h(f)/h0(f) where h0(f) is some fiducial waveform and h(f) correspond to parameter combinations par
-    h0: fiducial waveform (it is important to pass one that is NOT shifted to the right merger time)
-    fbin: frequency bin edges
-    par is list of parameters: [Mc, eta, chieff, chia, Lam, dtc]
-    tc: best-fit time
-    """
+    #Now computing exact waveform at the new resolution 'res'
 
-    f = fbin  # only evaluate at frequency bin edges
-    h0_bin = h0[fbin_ind]
+    h2_0 = hf3hPN(fp, M, ETA, 0.0, s1z=S1Z, s2z=S2Z, Lam=LAM)
+    h2_1 = hf3hPN(fp, M, ETA, 0.5*np.pi, s1z=S1Z, s2z=S2Z, Lam=LAM)
+    h20 = h2_0*np.exp(-2.0j*np.pi*fp*TC1)
+    h21 = h2_1*np.exp(-2.0j*np.pi*fp*TC1)
+    psd = sh(fp)
 
-    Mc, eta, chieff, chia, Lam, dtc = par
-    M = Mc / eta ** 0.6
-    delta = np.sqrt(1.0 - 4.0 * eta)
-    chis = chieff - delta * chia
-    s1z = chis + chia
-    s2z = chis - chia
+    a = np.absolute(overlap(h20,h20,fp))
+    b = np.absolute(overlap(h21,h21,fp))
+    c = np.absolute(overlap(h_int,h_int,fp))
+    d = np.absolute(overlap(h20,h_int,fp))
+    e = np.absolute(overlap(h21,h_int,fp))
 
-    # waveform ratio
-    r = hf3hPN(f, M, eta, 0.0, s1z=s1z, s2z=s2z, Lam=Lam) / h0_bin * np.exp(-2.0j * np.pi * f * dtc)
-    r0 = 0.5 * (r[:-1] + r[1:])
-    r1 = (r[1:] - r[:-1]) / (f[1:] - f[:-1])
+    f = d/(np.sqrt(a)*np.sqrt(c))
+    g = e/(np.sqrt(b)*np.sqrt(c))
 
-    return np.array([r0, r1], dtype=np.complex128)
+    h[l] = np.sqrt(f**2+g**2)
 
-
-# compute minus log-likelihood
-def lnlike(par, sdat, h0, fbin, fbin_ind, ndtct):
-    """
-    Return log of the likelihood function
-    par: list of parameters [Mc, eta, chieff, chia, Lam, dtc1, dtc2] (two time constants tc1 and tc2 for two detectors)
-    sdat: summary data
-    h0: a list of fiducial waveforms
-    fbin: bin edge frequencies
-    fbin_ind: bin edge positions in the full frequency grid
-    ndtct: number of detectors
-    """
-
-    lnl = 0.0
-
-    Mc, eta, chieff, chia, Lam = par[0:5]
-    dtc = par[5:]
-    par_notc = [Mc, eta, chieff, chia, Lam]
-
-    # relative waveform
-    rf = [compute_rf(par_notc + [dtc[k]], h0[k], fbin, fbin_ind) for k in range(ndtct)]
-
-    # Total log-likelihood is the sum of all detectors
-    for k in range(ndtct):
-        # The log-likelihood function is analytically maximized with respect to the phase constant and the overall amplitude
-        lnl -= 0.5 * mfSNR2_amp_phic_max_sdat(sdat, k, rf[k])
-
-    return lnl
-
-
-# ----------- find best-fit parameters using minimization ------------- #
-def get_best_fit(sdat, par, par_bounds, h0, fbin, fbin_ind, ndtct, maxiter=100, atol=1e-3, verbose=False):
-    """
-    Find the best-fit binary parameters that fit the data (with detector noise added)
-    sdat is the summary data
-    par_bounds is a list of allowed range for all parameters
-    """
-    # use differential evolution from scipy
-    #output = differential_evolution(lnlike, bounds=par_bounds, \
-    #                                args=(sdat, h0, fbin, fbin_ind, ndtct), atol=atol, maxiter=maxiter)
-    output = minimize(lnlike, par, bounds=par_bounds, args = (sdat, h0, fbin, fbin_ind, ndtct))
-    
-    res = output['x']
-    lnl = -output['fun']
-
-    # best-fit parameters
-    Mc_bf = res[0]
-    eta_bf = res[1]
-    chieff_bf = res[2]
-    chia_bf = res[3]
-    Lam_bf = res[4]
-    dtc_bf = res[5:]
-
-    # output best-fit parameters if requested
-    if verbose is True:
-        print('log-likelihood = ', lnl)
-        print('Mc_bf =', Mc_bf)
-        print('eta_bf =', eta_bf)
-        print('chieff_bf =', chieff_bf)
-        print('chia_bf =', chia_bf)
-        print('Lam_bf =', Lam_bf)
-        print('dtcbf =', dtc_bf)
-
-    return res
+pl.plot(MA,h)
+pl.savefig("figures/overlap_MC.pdf")
