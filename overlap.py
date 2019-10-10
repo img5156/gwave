57055061c17ac837edb7d07eaa6ba58efde4b9e6
img5156 <55452901+img5156@users.noreply.github.com>
2019-10-10 16:40:51 -0400

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -143,7 +143,7 @@ print(fp[1000])
 pl.plot(f[fbin_ind[0]:(fbin_ind[0]+int(1000*ad))],h_int2[fbin_ind[0]:(fbin_ind[0]+int(1000*ad))],'g')
 pl.plot(fp[:1000],h_int[:1000],'r')
 pl.savefig("figures/overlap_comp2.pdf")
-a = np.absolute(overlap(h1[fbin_ind[0]:fbin_ind[-1]],h1[fbin_ind[0]:],f[fbin_ind[0]:fbin_ind[-1]]))
+a = np.absolute(overlap(h1[fbin_ind[0]:fbin_ind[-1]],h1[fbin_ind[0]:fbin_ind[-1]],f[fbin_ind[0]:fbin_ind[-1]]))
 b = np.absolute(overlap(h_int2[fbin_ind[0]:fbin_ind[-1]],h_int2[fbin_ind[0]:fbin_ind[-1]],f[fbin_ind[0]:fbin_ind[-1]]))
 c = np.absolute(overlap(h_int2[fbin_ind[0]:fbin_ind[-1]],h1[fbin_ind[0]:fbin_ind[-1]],f[fbin_ind[0]:fbin_ind[-1]]))
 
